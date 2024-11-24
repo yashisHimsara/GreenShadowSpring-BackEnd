28a18a9b00344f0f6b1f45994a6b163fcdb9bd47
yashisHimsara <yashissenavirathna@gmail.com>
2024-11-25 00:35:56 +0530

    create mapping class

--- src/main/java/com/example/greenshadowbackend/util/Mapping.java
+++ src/main/java/com/example/greenshadowbackend/util/Mapping.java
@@ -1,9 +1,7 @@
 package com.example.greenshadowbackend.util;
 
-import com.example.greenshadowbackend.dto.impl.CropDto;
-import com.example.greenshadowbackend.dto.impl.FieldDto;
-import com.example.greenshadowbackend.entity.impl.CropEntity;
-import com.example.greenshadowbackend.entity.impl.FieldEntity;
+import com.example.greenshadowbackend.dto.impl.*;
+import com.example.greenshadowbackend.entity.impl.*;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,8 +23,54 @@ public class Mapping {
     public List<FieldDto> asFieldDtoList(List<FieldEntity> all) {
         return mapper.map(all, new TypeToken<List<FieldDto>>(){}.getType());
     }
+
     public CropDto toCropDto(CropEntity crop){
         return mapper.map(crop, CropDto.class);
     }
+    public CropEntity toCropEntity(CropDto crop){
+        return mapper.map(crop, CropEntity.class);
+    }
+    public List<CropDto> asCropDtoList(List<CropEntity> all){
+        return mapper.map(all, new TypeToken<List<CropDto>>(){}.getType());
+    }
 
+    public StaffDto toStaffDto(StaffEntity staff){
+        return mapper.map(staff, StaffDto.class);
+    }
+    public StaffEntity toStaffEntity(StaffDto staff){
+        return mapper.map(staff, StaffEntity.class);
+    }
+    public List<StaffDto> toStaffDtoList(List<StaffEntity> all){
+        return mapper.map(all, new TypeToken<List<StaffDto>>(){}.getType());
+    }
+
+    public EquipmentEntity toEquipmentEntity(EquipmentDto equipmentDto) {
+        return mapper.map(equipmentDto, EquipmentEntity.class);
+    }
+
+    public List<EquipmentDto> toEquipmentDTOList(List<EquipmentEntity> all) {
+        return mapper.map(all,new TypeToken<List<EquipmentDto>>(){}.getType());
+    }
+
+    public EquipmentDto toEquipmentDTO(EquipmentEntity equipmentEntity) {
+        return mapper.map(equipmentEntity,EquipmentDto.class);
+    }
+
+    public MonitoringLogEntity toLogEntity(MonitoringLogDto monitoringLogDTO) {
+        return mapper.map(monitoringLogDTO,MonitoringLogEntity.class);
+    }
+
+    public List<MonitoringLogDto> asLogDtoList(List<MonitoringLogEntity> all) {
+        return mapper.map(all,new TypeToken<List<MonitoringLogDto>>(){}.getType());
+    }
+
+    public VehicleDto tovehicleDto(VehicleEntity vehicle){
+        return mapper.map(vehicle, VehicleDto.class);
+    }
+    public  VehicleEntity toVehicleEntity(VehicleDto vehicle){
+        return mapper.map(vehicle, VehicleEntity.class);
+    }
+    public VehicleDto toVehicleDto(VehicleEntity vehicleEntity) {
+        return mapper.map(vehicleEntity,VehicleDto.class);
+    }
 }
